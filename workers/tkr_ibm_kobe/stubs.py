3befb94e6aaa9c9868ec678a25261809ef88fe20
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-29 23:38:19 +0900

    Make TranspileInformation Opaque

--- workers/tkr_ibm_kobe/stubs.py
+++ workers/tkr_ibm_kobe/stubs.py
@@ -1,18 +1,19 @@
 """Code generated from tkr_ibm_kobe namespace. Please do not edit."""
 
-from typing import NamedTuple
+from typing import NamedTuple, Protocol
 from tierkreis.controller.data.models import TKR, OpaqueType
+from tierkreis.controller.data.types import Struct
 
 
-class TranspileInformation(NamedTuple):
-    config: TKR[OpaqueType["qiskit_ibm_runtime.models.backend_configuration.QasmBackendConfiguration"]]  # noqa: F821 # fmt: skip
-    props: TKR[OpaqueType["qiskit_ibm_runtime.models.backend_properties.BackendProperties"]]  # noqa: F821 # fmt: skip
+class TranspileInformation(Struct, Protocol):
+    config: OpaqueType["qiskit_ibm_runtime.models.backend_configuration.QasmBackendConfiguration"]  # noqa: F821 # fmt: skip
+    props: OpaqueType["qiskit_ibm_runtime.models.backend_properties.BackendProperties"]  # noqa: F821 # fmt: skip
 
 
 class get_backend_info(NamedTuple):
     @staticmethod
-    def out() -> type[TranspileInformation]:  # noqa: F821 # fmt: skip
-        return TranspileInformation  # noqa: F821 # fmt: skip
+    def out() -> type[TKR[TranspileInformation]]:  # noqa: F821 # fmt: skip
+        return TKR[TranspileInformation]  # noqa: F821 # fmt: skip
 
     @property
     def namespace(self) -> str:
