f512db5dfb4d6b331247aa99bd03b96cdee1956a
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 15:37:23 +0000

    Update fugaku.sh

--- workers/tkr_ibm_kobe/scripts/fugaku.sh
+++ workers/tkr_ibm_kobe/scripts/fugaku.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -x
+set -eou pipefail
 
 source $(dirname $0)/backend_setup.sh ibm-kobe-dacc
-uv run g++ -O3 -Wall -shared -std=c++11 -fPIC tkr_sqcsub.cpp -o tkr_sqcsub$(uv run python -m pybind11 --extension-suffix) $(uv run python -m pybind11 --includes) ${SQC_COMPILE_OPTIONS}
+uv run g++ -O3 -Wall -shared -std=c++11 -fPIC $(uv run python -m pybind11 --includes) tkr_sqcsub.cpp -o tkr_sqcsub$(uv run python -m pybind11 --extension-suffix) ${SQC_COMPILE_OPTIONS}
