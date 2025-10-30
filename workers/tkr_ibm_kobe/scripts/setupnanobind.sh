6e705b8a58a3ae1840225dfaab36cbbb02e0c34a
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 11:24:49 +0000

    Update setupnanobind.sh

--- workers/tkr_ibm_kobe/scripts/setupnanobind.sh
+++ workers/tkr_ibm_kobe/scripts/setupnanobind.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cmake -S . -B build -DPython_EXECUTABLE=./.venv/bin/python -DPYTHON_INCLUDE_DIR=$(python3.11 -c "import sysconfig; print(sysconfig.get_path('include'))")  \
-    -DPYTHON_LIBRARY=$(python3.11 -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+    -DPYTHON_LIBRARY=$(python3.11 -c "import distutils.sysconfig as sysconfig; import os; print(os.path.join(sysconfig.get_config_var('LIBDIR'), sysconfig.get_config_var('LDLIBRARY')))") \
     -DPYTHON_EXECUTABLE:FILEPATH=`which python3.11`
 cmake --build build
