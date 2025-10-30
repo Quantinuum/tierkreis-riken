fe16be67ae2b2b58b8a5bd1f1b68c094d090595a
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 11:39:29 +0000

    Update setupnanobind.sh

--- workers/tkr_ibm_kobe/scripts/setupnanobind.sh
+++ workers/tkr_ibm_kobe/scripts/setupnanobind.sh
@@ -2,6 +2,6 @@
 
 source $(dirname $0)/backend_setup.sh ibm-kobe-dacc
 
-rm -rf ./build
+# rm -rf ./build
 uv run cmake -S . -B build -DPython_EXECUTABLE=./.venv/bin/python
-uv run cmake --build build -DCMAKE_CXX_FLAGS="${SQC_COMPILE_OPTIONS}"
+uv run cmake --build build -DCMAKE_CXX_FLAGS="'${SQC_COMPILE_OPTIONS}'"
