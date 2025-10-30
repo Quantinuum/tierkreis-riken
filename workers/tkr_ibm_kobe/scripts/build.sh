4fc2e4bfa7846b2c05eb2df78e16157fe32da65f
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 15:31:05 +0000

    Update scripts

--- workers/tkr_ibm_kobe/scripts/build.sh
+++ workers/tkr_ibm_kobe/scripts/build.sh
@@ -3,5 +3,5 @@ DIR=$( dirname "${BASH_SOURCE[0]}" )
 source $DIR/backend_setup.sh ibm-kobe-dacc
 
 #gcc $DIR/../src/tkr_sqcsub.c ${SQC_COMPILE_OPTIONS}
-gcc -c -fPIC $DIR/../src/tkr_sqcsub.c -o $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
+gcc -c -fPIC $DIR/../tkr_sqcsub.c -o $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
 gcc -shared -o $DIR/../build/tkr_sqcsub.so $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
