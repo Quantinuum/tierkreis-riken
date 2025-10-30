cffe60b33b7920ca82c9e2e80864bc622720aa14
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 15:19:56 +0000

    Fix cli options

--- workers/tkr_ibm_kobe/scripts/pybind.sh
+++ workers/tkr_ibm_kobe/scripts/pybind.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-uv run g++ -O3 -Wall -shared -std=c++11 -fPIC $(python -m pybind11 --includes) example.cpp -o example$(python -m pybind11 --extension-suffix)
+uv run g++ -O3 -Wall -undefined dynamic_lookup -shared -std=c++11 -fPIC $(uv run python -m pybind11 --includes) tkr_sqcsub.cpp -o example$(uv run python -m pybind11 --extension-suffix) ${SQC_COMPILE_OPTIONS}
