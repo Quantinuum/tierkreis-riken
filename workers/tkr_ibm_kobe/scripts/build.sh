d65cafb7df01069d3790e51702e0dd2142473841
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-30 16:16:42 +0000

    Try subprocess

--- workers/tkr_ibm_kobe/scripts/build.sh
+++ workers/tkr_ibm_kobe/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR=$( dirname "${BASH_SOURCE[0]}" )
 source $DIR/backend_setup.sh ibm-kobe-dacc
-echo ${SQC_COMPILE_OPTIONS}
-gcc -std=c++11 $DIR/../tkr_sqcsub.cpp ${SQC_COMPILE_OPTIONS}
-# gcc -c -fPIC $DIR/../tkr_sqcsub.c -o $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
+
+gcc $DIR/../src/tkr_sqcsub.c ${SQC_COMPILE_OPTIONS}
+# gcc -c -fPIC $DIR/../src/tkr_sqcsub.c -o $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
 # gcc -shared -o $DIR/../build/tkr_sqcsub.so $DIR/../build/tkr_sqcsub.o ${SQC_COMPILE_OPTIONS}
---
+++ workers/tkr_ibm_kobe/tkr_sqcsub.c
@@ -0,0 +1,33 @@
+#include "sqc_api.h"
+#include "sqc_ecode.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QASM_LEN (1024 * 1024)
+
+void get_transpile_info(char *config_file, char *props_file)
+{
+    sqcInitOptions *init_options = sqcMallocInitOptions();
+    init_options->use_qiskit = 0;
+    sqcInitialize(init_options);
+
+    sqcQC *qcir = sqcQuantumCircuit(1);
+    sqcIbmdTranspileInfo(qcir, SQC_RPC_SCHED_QC_TYPE_IBM_DACC);
+
+    props = qcir->backend_config_json;
+    config = qcir->backend_props_json;
+
+    // File *outputFile = fopen("output", "a");
+    printf("%s\n", qcir->backend_config_json);
+
+    // File *outputFile = fopen("output", "a");
+    printf("%s\n", qcir->backend_props_json);
+}
+
+int main(int argc, char *argv[])
+{
+    get_transpile_info();
+
+    return 0;
+}
