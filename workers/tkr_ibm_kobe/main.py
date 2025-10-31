6d94ae6261cd77c527bd6476b772be9f32934fbf
Matthew Burke <matthew.burke@quantinuum.com>
2025-10-31 10:14:18 +0000

    Add compilation task

--- workers/tkr_ibm_kobe/main.py
+++ workers/tkr_ibm_kobe/main.py
@@ -1,3 +1,5 @@
+from pytket._tket.circuit import Circuit
+from pytket.extensions.qiskit.backends.ibm import IBMQBackend
 from tierkreis import Worker
 
 from tkr_sqcsub import TranspileInfo, get_transpile_info_inner
@@ -9,3 +11,10 @@ worker = Worker("tkr_ibm_kobe")
 @worker.task()
 def get_transpile_info() -> TranspileInfo:
     return get_transpile_info_inner()
+
+
+@worker.task()
+def compile_using_info(info: TranspileInfo, circuit: Circuit) -> Circuit:
+    base_pass = IBMQBackend.default_compilation_pass_offline(info.config, info.props)
+    base_pass.apply(circuit)
+    return circuit
