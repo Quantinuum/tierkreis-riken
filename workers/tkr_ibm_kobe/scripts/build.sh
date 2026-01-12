02b937c60353b72ccdcdb87c8b03121847c42505
Matthew Burke <matthew.burke@quantinuum.com>
2026-01-12 11:20:56 +0000

    Update build.sh

--- workers/tkr_ibm_kobe/scripts/build.sh
+++ workers/tkr_ibm_kobe/scripts/build.sh
@@ -2,5 +2,6 @@
 set -euo pipefail
 
 source "$( dirname -- "${BASH_SOURCE[0]}" )"/backend_setup.sh ibm-kobe-dacc
+mkdir -p build
 gcc "$( dirname -- "${BASH_SOURCE[0]}" )"/../src/transpile_info.c -o build/transpile_info.o ${SQC_COMPILE_OPTIONS}
 
